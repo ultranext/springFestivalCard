0e8ffeac8b56867f6527e88c863204f7c784c501
ultranext <991390671@qq.com>
2016-07-28 21:49:03 +0800

    submit all files
    提交文件

---
+++ script/script.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+function $(str){
+	return document.querySelector(str);
+	}
+	//添加触摸事件监听音乐播放
+$("#music").addEventListener("touchstart",function(){
+	if($("#media").paused){
+		$("#media").play();
+		$(".play").style.animationPlayState="running";
+		}
+	else{$("#media").pause();$(".play").style.animationPlayState="paused";}
+	},false);
+	$("#media").addEventListener("ended",function(){
+		$("#media").pause();$(".play").style.animationPlayState="paused";
+		},false)
+	//添加触摸事件控制音乐淡入淡出
+	$("#page1").addEventListener("touchstart",function(){
+		$("#page1").style.display="none";
+		$("#page2").style.display="block";
+		var timer=setInterval(function(){
+			$("#page2").classList.add("fadeout");
+			$("#page3").classList.add("fadein");
+		$("#page3").style.display="block";},5500);
+		
+		},false)
+		document.addEventListener("touchmove",function(event){console.log(event.touches[0].screenX);console.log(event.target.screenY);},false)
