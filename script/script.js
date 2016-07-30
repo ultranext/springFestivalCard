301310317e45207db855bd0691ae6aaad00425c4
ultranext <991390671@qq.com>
2016-07-30 13:10:50 +0800

    修复兼容性问题
    碟片控制效果兼容性，对于支持animationplaystate则通过该属性控制，不支持则改用移除play属性

--- script/script.js
+++ script/script.js
@@ -3,13 +3,30 @@ function $(str){
 	return document.querySelector(str);
 	}
 	//添加触摸事件监听音乐播放
+	$("#media").addEventListener("ended",function(){
+		if($("#disc").style.animationPlayState!=undefined){
+			$("#disc").style.animationPlayState="running";
+		}else{
+			$("#disc").setAttribute("class","");
+		}
+
+	},false);
 $("#music").addEventListener("touchstart",function(){
 	if($("#media").paused){
 		$("#media").play();
-		$(".play").style.animationPlayState="running";
+		//$(".play").style.animationPlayState="running";
+		if($("#disc").style.animationPlayState!=undefined){
+			$("#disc").style.animationPlayState="running";
+		}else{
+			$("#disc").setAttribute("class","play");
 		}
-	else{$("#media").pause();$(".play").style.animationPlayState="paused";}
-	},false);
+		}
+	else{$("#media").pause();if($(".play").style.animationPlayState!=undefined){
+			$("#disc").style.animationPlayState="paused";
+		}else{
+			$("#disc").setAttribute("class","");
+		}
+	}},false);
 	$("#media").addEventListener("ended",function(){
 		$("#media").pause();$(".play").style.animationPlayState="paused";
 		},false)
@@ -23,4 +40,5 @@ $("#music").addEventListener("touchstart",function(){
 		$("#page3").style.display="block";},5500);
 		
 		},false)
+
 		document.addEventListener("touchmove",function(event){console.log(event.touches[0].screenX);console.log(event.target.screenY);},false)
